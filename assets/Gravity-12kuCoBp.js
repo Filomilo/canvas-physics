69d598b9e51f2190713e99d475918ef718f84d2d
Filomilo <fborowiec@wp.pl>
2024-10-21 12:47:47 +0200

    Updates

---
+++ assets/Gravity-12kuCoBp.js
@@ -0,0 +1,1 @@
+import{G as a,V as o,b as e}from"./GameComponent.vue_vue_type_style_index_0_lang-C4RZRmDn.js";class i extends a{constructor(){super()}affect(t){t.addVelocity(new o(0,e.Gravity*e.SimulationTimeStep))}}export{i as G};
